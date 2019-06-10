f9d35c48a320dcf9751e6e28ed301582bd4ecd3a
unknown <r.vandegraaf45@gmail.com>
2019-06-10 13:49:41 +0200

    update

---
+++ src/Form/ExtrasType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Extras;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ExtrasType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('omschrijving')
+            ->add('meerprijs')
+            ->add('kamerid')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Extras::class,
+        ]);
+    }
+}
